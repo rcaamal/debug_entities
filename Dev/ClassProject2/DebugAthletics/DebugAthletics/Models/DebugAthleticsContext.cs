4ccf4b1283a037f32cc1e95d3a608481eb489425
Ian Oberst <ioberst17@mail.wou.edu>
2020-02-03 16:11:37 -0800

    Added picture functionality

---
+++ Dev/ClassProject2/DebugAthletics/DebugAthletics/Models/DebugAthleticsContext.cs
@@ -0,0 +1,46 @@
+namespace DebugAthletics
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class DebugAthleticsContext : DbContext
+    {
+        public DebugAthleticsContext()
+            : base("name=DebugAthleticsContext")
+        {
+        }
+
+        public virtual DbSet<Administrator> Administrators { get; set; }
+        public virtual DbSet<Athlete> Athletes { get; set; }
+        public virtual DbSet<Coach> Coaches { get; set; }
+        public virtual DbSet<Result> Results { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Athlete>()
+                .HasMany(e => e.Results)
+                .WithRequired(e => e.Athlete)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Coach>()
+                .HasMany(e => e.Athletes)
+                .WithRequired(e => e.Coach)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<User>()
+                .HasMany(e => e.Administrators)
+                .WithRequired(e => e.User)
+                .HasForeignKey(e => e.UserInfo)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<User>()
+                .HasMany(e => e.Coaches)
+                .WithRequired(e => e.User)
+                .HasForeignKey(e => e.UserInfo)
+                .WillCascadeOnDelete(false);
+        }
+    }
+}
