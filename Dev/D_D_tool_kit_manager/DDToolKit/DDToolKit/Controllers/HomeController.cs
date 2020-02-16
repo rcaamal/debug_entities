d454ec7f8ea59a202c9e56967391b936a27ed4b3
Zeed Alsuwayyigh <zalsuwayyigh18@mail.wou.edu>
2020-02-15 21:57:00 -0800

    Added Captcha and edited the up script, the regeister controller is not working. i ran the code before i added my work, but it was not working

--- Dev/D_D_tool_kit_manager/DDToolKit/DDToolKit/Controllers/HomeController.cs
+++ Dev/D_D_tool_kit_manager/DDToolKit/DDToolKit/Controllers/HomeController.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using CaptchaMvc.HtmlHelpers;
 namespace DDToolKit.Controllers
+
 {
     public class HomeController : Controller
     {
