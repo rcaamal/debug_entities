7577d67c1d2f7fcf267ac1f155cb248d63f1e293
Randy Caamal <43390072+rcaamal@users.noreply.github.com>
2020-02-17 13:02:10 -0800

    added the contact form

---
+++ Dev/D_D_tool_kit_manager/DDToolKit/DDToolKit/Views/Home/script.js
@@ -0,0 +1,8 @@
+﻿function validation()
+{
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+    var message = document.getElementById("message").value;
+    var error_message = document.getElementById("error_message");
+ }
