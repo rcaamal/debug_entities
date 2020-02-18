e34400cffe9e827e410659d80dd7e882be72bdad
Troy <tonyhawk_95@yahoo.com>
2020-02-17 16:43:09 -0800

    Got list of monsters styled and working correctly.

---
+++ Dev/D_D_tool_kit_manager/DDToolKit/DDToolKit/Controllers/CreaturesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DDToolKit.DAL;
+using DDToolKit.Models;
+
+namespace DDToolKit.Controllers
+{
+    public class CreaturesController : Controller
+    {
+        private Monsters db = new Monsters();
+
+        // GET: Creatures
+        public ActionResult Index()
+        {
+            return View(db.Creatures.ToList());
+        }
+
+        // GET: Creatures/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Creature creature = db.Creatures.Find(id);
+            if (creature == null)
+            {
+                return HttpNotFound();
+            }
+            return View(creature);
+        }
+
+        // GET: Creatures/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Creatures/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Size,Type,Aligment,ArmorClass,HitPoints,HitDice,Strength,Dexterity,Constitution,Intelligence,Wisdom,Charisma,ChallangeRating,Speed,Senses,SubType,Languages,Proficiencies,DamageResistance,DamageVulnerability,DamageImmunity,ConditionImmunity,SpecialAbility,Actions,LegendaryActions")] Creature creature)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Creatures.Add(creature);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(creature);
+        }
+
+        // GET: Creatures/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Creature creature = db.Creatures.Find(id);
+            if (creature == null)
+            {
+                return HttpNotFound();
+            }
+            return View(creature);
+        }
+
+        // POST: Creatures/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Size,Type,Aligment,ArmorClass,HitPoints,HitDice,Strength,Dexterity,Constitution,Intelligence,Wisdom,Charisma,ChallangeRating,Speed,Senses,SubType,Languages,Proficiencies,DamageResistance,DamageVulnerability,DamageImmunity,ConditionImmunity,SpecialAbility,Actions,LegendaryActions")] Creature creature)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(creature).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(creature);
+        }
+
+        // GET: Creatures/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Creature creature = db.Creatures.Find(id);
+            if (creature == null)
+            {
+                return HttpNotFound();
+            }
+            return View(creature);
+        }
+
+        // POST: Creatures/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Creature creature = db.Creatures.Find(id);
+            db.Creatures.Remove(creature);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
