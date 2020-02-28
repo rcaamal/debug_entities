dca7e86d19c73d2cf951d14fbebea71bf904c138
Troy <tonyhawk_95@yahoo.com>
2020-02-27 17:12:51 -0800

    Got sql seed working

--- Dev/D_D_tool_kit_manager/DDToolKit/DDToolKit/App_Data/up.sql
+++ Dev/D_D_tool_kit_manager/DDToolKit/DDToolKit/App_Data/up.sql
@@ -1,4 +1,4 @@
-﻿create table [dbo].[Creatures](
+﻿/*create table [dbo].[Creatures](
 	[ID] INT IDENTITY (1,1) NOT NULL,
 	[Name] NvarChar(30) Not NULL,
 	[Size] NvarChar(15) Not NULL,
@@ -8,24 +8,55 @@
 	[ArmorClass] Int Not Null,
 	[HitPoints] Int Not Null,
 	[HitDice] NvarChar(10) Not Null,
+	[Speed] NvarChar(500) Not Null,
 	[Strength] int Not Null,
 	[Dexterity] int Not Null,
 	[Constitution] int Not Null,
 	[Intelligence] int Not Null,
 	[Wisdom] int Not Null,
 	[Charisma] int Not Null,
-	[Languages] NvarChar(500),
-	[ChallangeRating] Float Not Null,
-	[Speed] NvarChar(500) Not Null,
-	[Proficiencies] NvarChar(500),
-	[DamageResistance] NvarChar(500),
+	[Proficiencies] NvarChar(MAX),
 	[DamageVulnerability] NvarChar(500),
+	[DamageResistance] NvarChar(500),
 	[DamageImmunity] NvarChar(500),
 	[ConditionImmunity] NvarChar(500),
 	[Senses] NvarChar(500) Not Null,
-	[SpecialAbility] NvarChar(500),
+	[Languages] NvarChar(500),
+	[ChallangeRating] Float Not Null,
+	[SpecialAbility] NvarChar(MAX),
 	[Actions] NVarChar(MAX),
-	[LegendaryActions] NVarChar(MAX)
+	[LegendaryActions] NVarChar(MAX),
+	[Reactions] NVarChar(500)
+);*/
+
+CREATE TABLE [dbo].[Creatures] (
+    [name] NVARCHAR(25),
+    [size] NVARCHAR(10),
+    [type] NVARCHAR(20),
+    [subtype] NVARCHAR(12),
+    [alignment] NVARCHAR(40),
+    [armor_class] INT,
+    [hit_points] INT,
+    [hit_dice] NVARCHAR(5),
+    [speed] NVARCHAR(73),
+    [strength] INT,
+    [dexterity] INT,
+    [constitution] INT,
+    [intelligence] INT,
+    [wisdom] INT,
+    [charisma] INT,
+    [proficiencies] NVARCHAR(794),
+    [damage_vulnerabilities] NVARCHAR(57),
+    [damage_resistances] NVARCHAR(132),
+    [damage_immunities] NVARCHAR(115),
+    [condition_immunities] NVARCHAR(523),
+    [senses] NVARCHAR(102),
+    [languages] NVARCHAR(91),
+    [challenge_rating] NUMERIC(4, 2),
+    [special_abilities] NVARCHAR(MAX),
+    [actions] NVARCHAR(3863),
+    [legendary_actions] NVARCHAR(1559),
+    [reactions] NVARCHAR(468)
 );
 
 CREATE TABLE [dbo].[AspNetRoles](
