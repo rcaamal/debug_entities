acd427205ca0908d3077a9edd7830eaca8a092a3
Randy Caamal <43390072+rcaamal@users.noreply.github.com>
2020-03-04 22:09:10 -0800

    got google working

--- Dev/D_D_tool_kit_manager/DDToolKit/DDToolKit/App_Start/Startup.Auth.cs
+++ Dev/D_D_tool_kit_manager/DDToolKit/DDToolKit/App_Start/Startup.Auth.cs
@@ -51,23 +51,7 @@ namespace DDToolKit
             //app.UseMicrosoftAccountAuthentication(
             //    clientId: "",
             //    clientSecret: "");
-            app.UseTwitterAuthentication(new TwitterAuthenticationOptions
-            {
-                ConsumerKey = "f1Gl8VSB9tEC1xllIhRKBcGDs",
-                ConsumerSecret = "cZ2sBrzEV2eSxYCwUsa8ZP1zSd4wWDdrtYmdNU6lEeRYDiPqif",
-                BackchannelCertificateValidator = new CertificateSubjectKeyIdentifierValidator(new[]
-                {
-                    "A5EF0B11CEC04103A34A659048B21CE0572D7D47", // VeriSign Class 3 Secure Server CA - G2
-                    "0D445C165344C1827E1D20AB25F40163D8BE79A5", // VeriSign Class 3 Secure Server CA - G3
-                    "7FD365A7C2DDECBBF03009F34339FA02AF333133", // VeriSign Class 3 Public Primary Certification Authority - G5
-                    "39A55D933676616E73A761DFA16A7E59CDE66FAD", // Symantec Class 3 Secure Server CA - G4
-                    "5168FF90AF0207753CCCD9656462A212B859723B", //DigiCert SHA2 High Assurance Server C‎A 
-                    "B13EC36903F8BF4701D498261A0802EF63642BC3" //DigiCert High Assurance EV Root CA
-                })
-            });
-
-
-
+           
             //app.UseTwitterAuthentication(
             //consumerKey: "f1Gl8VSB9tEC1xllIhRKBcGDs",
             //consumerSecret: "CZ2sBrzEV2eSxYCwUsa8ZP1zSd4wWDdrtYmdNU6lEeRYDiPqif");
@@ -76,11 +60,14 @@ namespace DDToolKit
             //   appId: "",
             //   appSecret: "");
 
-            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+
+
+
+           app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
             {
-            ClientId = "721125938004-uc26arkuu3lo9s5klojf88pn2ttpvb9d.apps.googleusercontent.com",
-           ClientSecret = "VBLKNwkAh-4MQXjsqN3zvTc8"
-            });
+           ClientId = "510401239922-mp9dfnffurm373n10rstle4ohs3du5od.apps.googleusercontent.com",
+           ClientSecret = "p5plTnlwA_EV6_l6rCoO59a7"
+           });
         }
     }
 }
