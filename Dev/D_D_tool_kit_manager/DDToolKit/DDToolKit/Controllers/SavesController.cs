7ca329bb52cd18004b03acf333aea3b671e9e8dd
Ian Oberst <ioberst17@mail.wou.edu>
2020-03-09 15:52:14 -0700

    Should be done

--- Dev/D_D_tool_kit_manager/DDToolKit/DDToolKit/Controllers/SavesController.cs
+++ Dev/D_D_tool_kit_manager/DDToolKit/DDToolKit/Controllers/SavesController.cs
@@ -9,6 +9,7 @@ using System.Web.Mvc;
 using DDToolKit.DAL;
 using DDToolKit.Models;
 using DDToolKit.DAL;
+using Microsoft.AspNet.Identity;
 
 namespace DDToolKit.Controllers
 {
@@ -43,6 +44,7 @@ namespace DDToolKit.Controllers
         // GET: Saves/Create
         public ActionResult Create()
         {
+            ViewBag.Monsters = new SelectList(dbMonsters.Creatures, "Name", "Name");
             return View();
         }
 
@@ -55,7 +57,6 @@ namespace DDToolKit.Controllers
         {
 
             save.OwnerID = User.Identity.GetUserId();
-            save.Monsters = "";
             if (ModelState.IsValid)
             {
                 db.Saves.Add(save);
@@ -133,7 +134,7 @@ namespace DDToolKit.Controllers
         }
         public ActionResult Game(int? id)
         {
-            
+
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
@@ -141,113 +142,112 @@ namespace DDToolKit.Controllers
 
             Monsters db2 = new Monsters();
             string monsters = (from saves in db.Saves
-                            where (saves.ID == id)
-                            select saves.Monsters).Single();
-
-            string size = (from creatures in db2.Creatures
-                            where (creatures.Name == monsters)
-                            select creatures.Size).Single();
-            string type = (from creatures in db2.Creatures
-                           where (creatures.Name == monsters)
-                           select creatures.Type).Single();
-            string stype = (from creatures in db2.Creatures
-                           where (creatures.Name == monsters)
-                           select creatures.SubType).Single();
-            string Align = (from creatures in db2.Creatures
-                            where (creatures.Name == monsters)
-                            select creatures.Aligment).Single();
-            int ac = (from creatures in db2.Creatures
+                               where (saves.ID == id)
+                               select saves.Monsters).Single();
+            if(monsters != "" && monsters != null)
+            {
+                string size = (from creatures in db2.Creatures
+                               where (creatures.Name == monsters)
+                               select creatures.Size).Single();
+                string type = (from creatures in db2.Creatures
+                               where (creatures.Name == monsters)
+                               select creatures.Type).Single();
+                string stype = (from creatures in db2.Creatures
+                                where (creatures.Name == monsters)
+                                select creatures.Subtype).Single();
+                string Align = (from creatures in db2.Creatures
+                                where (creatures.Name == monsters)
+                                select creatures.Alignment).Single();
+                int? ac = (from creatures in db2.Creatures
                            where (creatures.Name == monsters)
                            select creatures.ArmorClass).Single();
-            int hp = (from creatures in db2.Creatures
+                int? hp = (from creatures in db2.Creatures
                            where (creatures.Name == monsters)
                            select creatures.HitPoints).Single();
-            string hd = (from creatures in db2.Creatures
-                      where (creatures.Name == monsters)
-                      select creatures.HitDice).Single();
-            int str = (from creatures in db2.Creatures
-                      where (creatures.Name == monsters)
-                      select creatures.Strength).Single();
-            int dex = (from creatures in db2.Creatures
-                      where (creatures.Name == monsters)
-                      select creatures.Dexterity).Single();
-            int con = (from creatures in db2.Creatures
-                      where (creatures.Name == monsters)
-                      select creatures.Constitution).Single();
-            int intel = (from creatures in db2.Creatures
-                      where (creatures.Name == monsters)
-                      select creatures.Intelligence).Single();
-            int wis = (from creatures in db2.Creatures
-                         where (creatures.Name == monsters)
-                         select creatures.Wisdom).Single();
-            int cha = (from creatures in db2.Creatures
-                         where (creatures.Name == monsters)
-                         select creatures.Charisma).Single();
-            string lang = (from creatures in db2.Creatures
-                         where (creatures.Name == monsters)
-                         select creatures.Languages).Single();
-            double chalrating = (from creatures in db2.Creatures
-                         where (creatures.Name == monsters)
-                         select creatures.ChallangeRating).Single();
-            string speed = (from creatures in db2.Creatures
-                         where (creatures.Name == monsters)
-                         select creatures.Speed).Single();
-            string prof = (from creatures in db2.Creatures
-                         where (creatures.Name == monsters)
-                         select creatures.Proficiencies).Single();
-            string Dresist = (from creatures in db2.Creatures
-                           where (creatures.Name == monsters)
-                           select creatures.DamageResistance).Single();
-            string Dvuln = (from creatures in db2.Creatures
-                           where (creatures.Name == monsters)
-                           select creatures.DamageVulnerability).Single();
-            string Dinvuln = (from creatures in db2.Creatures
-                           where (creatures.Name == monsters)
-                           select creatures.DamageImmunity).Single();
-            string Cinvuln = (from creatures in db2.Creatures
+                string hd = (from creatures in db2.Creatures
+                             where (creatures.Name == monsters)
+                             select creatures.HitDice).Single();
+                int? str = (from creatures in db2.Creatures
+                            where (creatures.Name == monsters)
+                            select creatures.Strength).Single();
+                int? dex = (from creatures in db2.Creatures
+                            where (creatures.Name == monsters)
+                            select creatures.Dexterity).Single();
+                int? con = (from creatures in db2.Creatures
+                            where (creatures.Name == monsters)
+                            select creatures.Constitution).Single();
+                int? intel = (from creatures in db2.Creatures
                               where (creatures.Name == monsters)
-                              select creatures.ConditionImmunity).Single();
-            string senses = (from creatures in db2.Creatures
-                           where (creatures.Name == monsters)
-                           select creatures.Senses).Single();
-            string special = (from creatures in db2.Creatures
-                           where (creatures.Name == monsters)
-                           select creatures.SpecialAbility).Single();
-            string actions = (from creatures in db2.Creatures
-                           where (creatures.Name == monsters)
-                           select creatures.Actions).Single();
-            string legactions = (from creatures in db2.Creatures
-                           where (creatures.Name == monsters)
-                           select creatures.LegendaryActions).Single();
-
+                              select creatures.Intelligence).Single();
+                int? wis = (from creatures in db2.Creatures
+                            where (creatures.Name == monsters)
+                            select creatures.Wisdom).Single();
+                int? cha = (from creatures in db2.Creatures
+                            where (creatures.Name == monsters)
+                            select creatures.Charisma).Single();
+                string lang = (from creatures in db2.Creatures
+                               where (creatures.Name == monsters)
+                               select creatures.Languages).Single();
+                decimal? chalrating = (from creatures in db2.Creatures
+                                       where (creatures.Name == monsters)
+                                       select creatures.ChallengeRating).Single();
+                string speed = (from creatures in db2.Creatures
+                                where (creatures.Name == monsters)
+                                select creatures.Speed).Single();
+                string prof = (from creatures in db2.Creatures
+                               where (creatures.Name == monsters)
+                               select creatures.Proficiencies).Single();
+                string Dresist = (from creatures in db2.Creatures
+                                  where (creatures.Name == monsters)
+                                  select creatures.DamageResistances).Single();
+                string Dvuln = (from creatures in db2.Creatures
+                                where (creatures.Name == monsters)
+                                select creatures.DamageVulnerabilities).Single();
+                string Dinvuln = (from creatures in db2.Creatures
+                                  where (creatures.Name == monsters)
+                                  select creatures.DamageImmunities).Single();
+                string Cinvuln = (from creatures in db2.Creatures
+                                  where (creatures.Name == monsters)
+                                  select creatures.ConditionImmunities).Single();
+                string senses = (from creatures in db2.Creatures
+                                 where (creatures.Name == monsters)
+                                 select creatures.Senses).Single();
+                string special = (from creatures in db2.Creatures
+                                  where (creatures.Name == monsters)
+                                  select creatures.SpecialAbilities).Single();
+                string actions = (from creatures in db2.Creatures
+                                  where (creatures.Name == monsters)
+                                  select creatures.Actions).Single();
+                string legactions = (from creatures in db2.Creatures
+                                     where (creatures.Name == monsters)
+                                     select creatures.LegendaryActions).Single();
+                ViewBag.size = size;
+                ViewBag.type = type;
+                ViewBag.stype = stype;
+                ViewBag.Align = Align;
+                ViewBag.ac = ac;
+                ViewBag.hp = hp;
+                ViewBag.hd = hd;
+                ViewBag.str = str;
+                ViewBag.dex = dex;
+                ViewBag.con = con;
+                ViewBag.intel = intel;
+                ViewBag.wis = wis;
+                ViewBag.cha = cha;
+                ViewBag.lang = lang;
+                ViewBag.chal = chalrating;
+                ViewBag.speed = speed;
+                ViewBag.prof = prof;
+                ViewBag.resist = Dresist;
+                ViewBag.vuln = Dvuln;
+                ViewBag.immune = Dinvuln;
+                ViewBag.invuln = Cinvuln;
+                ViewBag.senses = senses;
+                ViewBag.special = special;
+                ViewBag.actions = actions;
+                ViewBag.legact = legactions;
+            }
             ViewBag.name = monsters;
-            ViewBag.size = size;
-            ViewBag.type = type;
-            ViewBag.stype = stype;
-            ViewBag.Align = Align;
-            ViewBag.ac = ac;
-            ViewBag.hp = hp;
-            ViewBag.hd = hd;
-            ViewBag.str = str;
-            ViewBag.dex = dex;
-            ViewBag.con = con;
-            ViewBag.intel = intel;
-            ViewBag.wis = wis;
-            ViewBag.cha = cha;
-            ViewBag.lang = lang;
-            ViewBag.chal = chalrating;
-            ViewBag.speed = speed;
-            ViewBag.prof = prof;
-            ViewBag.resist = Dresist;
-            ViewBag.vuln = Dvuln;
-            ViewBag.immune = Dinvuln;
-            ViewBag.invuln = Cinvuln;
-            ViewBag.senses = senses;
-            ViewBag.special = special;
-            ViewBag.actions = actions;
-            ViewBag.legact = legactions;
-
-            //ViewBag.mons = test;
             return View(db.Players.ToList().Where(s => s.GameID == id));
         }
         public ActionResult CreatePlayer()
@@ -261,7 +261,7 @@ namespace DDToolKit.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult CreatePlayer(int id, [Bind(Include = "ID,OwnerID,GameID,Name,Size,Type,Aligment,ArmorClass,HitPoints,Strength,Dexterity,Constitution,Intelligence,Wisdom,Charisma,Languages,Speed,Proficiencies,DamageResistance,ConditionImmunity,Senses,SpecialAbility,Actions")] Player player)
         {
-            //int id = 1;
+            
             player.OwnerID = User.Identity.GetUserId();
             player.GameID = id;
             if (ModelState.IsValid)
@@ -270,16 +270,8 @@ namespace DDToolKit.Controllers
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-
             return View(player);
-
-        public ActionResult addMonsters()
-        {
-
-            ViewBag.names = new SelectList(dbMonsters.Creatures, "ID", "Name");
-
-            return View();
-
         }
     }
 }
+
