787cacb5e237e9b185470af92dc6f8b16d6bec83
Zeed Alsuwayyigh <zalsuwayyigh18@mail.wou.edu>
2020-05-04 21:38:40 -0700

    almost done

--- Dev/D_D_tool_kit_manager/DDToolKit/DDToolKit/Scripts/autoCompleteSpells.js
+++ Dev/D_D_tool_kit_manager/DDToolKit/DDToolKit/Scripts/autoCompleteSpells.js
@@ -1,3 +1,20 @@
 ﻿$(document).ready(function () {
-
+    $("#Name").autocomplete({
+        source: function (request, response) {
+            $.ajax({
+                url: "/Spell/Index",
+                type: "POST",
+                datatype: "json",
+                data: { Prefix: request.term },
+                success: function (data) {
+                    response($.map(data, function (data) {
+                        return { label: item.Name, value: item.Name };
+                    }))
+                }
+            })
+        },
+        messages: {
+            noResults: "", results: ""
+        }
+    });
 })
