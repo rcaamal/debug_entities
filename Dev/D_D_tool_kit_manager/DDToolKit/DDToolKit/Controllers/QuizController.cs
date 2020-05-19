9ce7b782ffcc558fca1f7242aaef83a15eca977b
Zeed Alsuwayyigh <zalsuwayyigh18@mail.wou.edu>
2020-05-18 19:06:35 -0700

    still working. nothing added yet

---
+++ Dev/D_D_tool_kit_manager/DDToolKit/DDToolKit/Controllers/QuizController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DDToolKit.Controllers
+{
+    public class QuizController : Controller
+    {
+        // GET: Quiz
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
