102e49181b5a5eac9065f69a84dc7ea6727a1475
Zeed Alsuwayyigh <zalsuwayyigh18@mail.wou.edu>
2020-05-19 21:19:32 -0700

    still working

--- Dev/D_D_tool_kit_manager/DDToolKit/DDToolKit/Controllers/QuizController.cs
+++ Dev/D_D_tool_kit_manager/DDToolKit/DDToolKit/Controllers/QuizController.cs
@@ -14,6 +14,67 @@ namespace DDToolKit.Controllers
             return View();
         }
 
+        /*public string CheckAnswer(int? answer, int? correctAnswer)
+        {
+
+            if (answer == correctAnswer)
+            {
+                return "Correct";
+            }
+
+            return "Incorrect";
+        }*/
+
+
+        [HttpPost]
+        public ActionResult Index(string input, string Ans1, string Ans2, int? Ans3, int? Ans4, int? Ans5, int? Ans6, int? Ans7, int? Ans8, int? Ans9
+            , int? Ans10, int? Ans11, int? Ans12, int? Ans13, int? Ans14, int? Ans15, int? Ans16, int? Ans17, int? Ans18, int? Ans19, int? Ans20)
+
+        {
+            
+            List<string> correctAnswers = new List<string>();
+            //correctAnswers.Add(Ans1, 3);
+            /*correctAnswers.Add(CheckAnswer(Ans2, 1));
+            correctAnswers.Add(CheckAnswer(Ans3, 1));
+            correctAnswers.Add(CheckAnswer(Ans4, 4));*/
+            Ans1 = "3";
+            Ans2 = "1";
+            string answer;
+            if(Ans1 == input)
+            {
+                answer = "Correct";
+            }
+            else
+            {
+                answer = "Incorrect";
+            }
+            if (Ans2 == input)
+            {
+                answer = "Correct";
+            }
+            correctAnswers.Add(Ans1);
+            correctAnswers.Add(Ans2);
+
+            /*correctAnswers.Add(Ans1);*/
+
+            int numOfCorrectAnswers = 0;
+            for (int  i =0; i < correctAnswers.Count(); i++)
+            {
+                if(correctAnswers[i] == "Correct")
+                {
+                    numOfCorrectAnswers++;
+
+                }
+            }
+
+            ViewBag.answers = correctAnswers;
+            
+            
+            return View();
+        }
+
+
+
 
     }
 }
---
+++ Dev/D_D_tool_kit_manager/DDToolKit/DDToolKit/Scripts/quiz.js
@@ -0,0 +1,10 @@
+﻿function quizCalculator() {
+    var results = 0;
+    if (document.querySelector('input[name="q1"]:checked').value == 2) {
+        totalScore = totalScore + 1;
+        document.getElementById("a1").innerHTML = "Correct";
+    }
+    else {
+        document.getElementById("a1").innerHTML = "Incorrect";
+    }
+}
